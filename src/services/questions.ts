3fcd7c2ae30ff22370c84e815f4591f7da6cc0c5
Rafael Olivay <rafaelsebastian@live.com.ar>
2023-11-17 22:03:49 -0300

    add url for gh-pages

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,8 +1,9 @@
 import { Question } from "../types";
 
 const services = {
-    questions: async (): Promise<Question[]> => {
-        const url = "http://localhost:5173/data.json";
+    questions: async (): Promise<Question[]> => {        
+        // const url = "http://localhost:5173/data.json";
+        const url = "https://rafaels85.github.io/javascript-quiz/";
         const res = await fetch(url);
         const { data } = await res.json();
         return data;
