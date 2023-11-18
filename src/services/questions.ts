e1e4ab84b2cb442ee99174d738ce9febb5653073
Rafael Olivay <rafaelsebastian@live.com.ar>
2023-11-17 22:40:33 -0300

    fix url

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -3,7 +3,7 @@ import { Question } from "../types";
 const services = {
     questions: async (): Promise<Question[]> => {        
         // const url = "http://localhost:5173/data.json";
-        const url = "https://rafaels85.github.io/javascript-quiz/";
+        const url = "https://rafaels85.github.io/javascript-quiz/data.json";
         const res = await fetch(url);
         const { data } = await res.json();
         return data;
